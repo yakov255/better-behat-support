3ccdb9a0e9b3cc897bd967d53b4e2424d4d07a13
GitHub Action <action@github.com>
2024-05-25 04:23:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/yakov255/betterbehatsupport/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yakov255/betterbehatsupport/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yakov255.betterbehatsupport.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yakov255.betterbehatsupport.MyBundle
+import com.github.yakov255.betterbehatsupport.services.MyProjectService
 import javax.swing.JButton
 
 

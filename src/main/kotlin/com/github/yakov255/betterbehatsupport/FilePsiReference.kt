7e2054e4801721cb15f3cb1763d38d6f08d194ea
Yakov Vladimirov <y.vladimirov@raketa.travel>
2024-05-26 21:55:34 +0500

    optimize code

--- src/main/kotlin/com/github/yakov255/betterbehatsupport/FilePsiReference.kt
+++ src/main/kotlin/com/github/yakov255/betterbehatsupport/FilePsiReference.kt
@@ -1,33 +1,17 @@
 package com.github.yakov255.betterbehatsupport
 
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiReferenceBase
 
 class FilePsiReference(element: PsiElement, private val fileName: String) : PsiReferenceBase<PsiElement>(element) {
 
     override fun resolve(): PsiElement? {
+        val project = myElement.project
+        val virtualFile = myElement.containingFile.virtualFile
 
-        val project: Project = myElement.project
-        val virtualFile: VirtualFile = myElement.containingFile.virtualFile
-
-        // Get the directory containing the current file
-        val currentDir: VirtualFile? = virtualFile.parent
-        currentDir ?: return null // Return null if there is no parent directory
-
-        // Find the "files" directory alongside the current file's directory
-        val filesDir: VirtualFile? = currentDir.findChild(Enum.directory)
-        filesDir ?: return null // Return null if the "files" directory does not exist
-
-        // Find the target file in the "files" directory
-        val targetFile: VirtualFile? = filesDir.findChild(fileName)
-        targetFile ?: return null // Return null if the target file does not exist
-
-        // Convert the VirtualFile to a PsiFile
-        val psiFile: PsiFile? = PsiManager.getInstance(project).findFile(targetFile)
-        return psiFile
+        return virtualFile.parent?.findChild(Enum.directory)?.findChild(fileName)?.let { targetFile ->
+            PsiManager.getInstance(project).findFile(targetFile)
+        }
     }
 }

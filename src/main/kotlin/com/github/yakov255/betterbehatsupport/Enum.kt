acadf1a815863f24f028fc1b02a71dc460829447
Yakov Vladimirov <y.vladimirov@raketa.travel>
2024-05-29 20:33:22 +0500

    Universal regex

--- src/main/kotlin/com/github/yakov255/betterbehatsupport/Enum.kt
+++ src/main/kotlin/com/github/yakov255/betterbehatsupport/Enum.kt
@@ -1,6 +1,6 @@
 package com.github.yakov255.betterbehatsupport
 
 object Enum {
-    const val pattern = "ответ содержит данные значения, аналогичные файлу (.+)"
+    const val pattern = "(\\S+\\.(json|xml|txt))"
     const val directory = "expected_responses"
 }
